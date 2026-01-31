2e0734be10da5a478ec4e6d3999a84930c4128a5
v_jiangrunxin <v_jiangrunxin@tal.com>
2026-02-01 06:02:38 +0800

    移除访问路径中的web-monitor前缀

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/web-monitor/',
+  base: '/',
   build: {
     outDir: 'dist'
   }
