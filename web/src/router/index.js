7fc11d5e8747f94dd09865566660ba06d4a69572
v_jiangrunxin <v_jiangrunxin@tal.com>
2026-02-01 11:51:40 +0800

    将Vue路由模式从history改为hash，解决页面刷新404问题

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Layout from '../layout/Layout.vue'
 import Dashboard from '../views/Dashboard.vue'
 import ApiList from '../views/ApiList.vue'
@@ -47,7 +47,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHashHistory(),
   routes
 })
 
